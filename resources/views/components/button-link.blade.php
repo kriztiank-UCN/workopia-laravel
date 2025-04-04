336235beab971922ae3c5786eb710ecca036863a
Kristian <10221425@ucn.dk>
2025-04-04 12:56:26 +0200

    Mobile Menu Nav Link & Toggle

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -4,10 +4,11 @@
     'bgClass' => 'bg-yellow-500',
     'hoverClass' => 'hover:bg-yellow-600',
     'textClass' => 'text-black',
+    'block' => false,
 ])
 
 <a href="{{ $url }}"
-  class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} rounded px-4 py-2 transition duration-300 hover:shadow-md">
+  class="{{ $bgClass }} {{ $hoverClass }} {{ $textClass }} {{ $block ? 'block' : '' }} rounded px-4 py-2 transition duration-300 hover:shadow-md">
   @if ($icon)
     <i class="fa fa-{{ $icon }} mr-1"></i>
   @endif
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,20 @@
-@props(['url' => '/', 'active' => false, 'icon' => null])
+@props(['url' => '/', 'active' => false, 'icon' => null, 'mobile' => null])
 
-<a href="{{ $url }}" class="{{ $active ? 'text-yellow-500 font-bold' : '' }} py-2 text-white hover:underline">
-  @if ($icon)
-  <i class="fa fa-gauge mr-1"></i>
-  @endif
-  {{ $slot }}
-</a>
+{{-- mobile menu --}}
+@if ($mobile)
+  <a href="{{ $url }}"
+    class="{{ $active ? 'text-yellow-500 font-bold' : '' }} block px-4 py-2 hover:bg-blue-700">
+    @if ($icon)
+      <i class="fa fa-{{ $icon }}" mr-1"></i>
+    @endif
+    {{ $slot }}
+  </a>
+@else
+  {{-- regular menu --}}
+  <a href="{{ $url }}" class="{{ $active ? 'text-yellow-500 font-bold' : '' }} py-2 text-white hover:underline">
+    @if ($icon)
+      <i class="fa fa-gauge mr-1"></i>
+    @endif
+    {{ $slot }}
+  </a>
+@endif
