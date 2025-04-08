d379d4b817632eab11f070a3b488c9d889e194ba
Kristian <10221425@ucn.dk>
2025-04-08 12:54:35 +0200

    Job Schema Update Migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,5 +10,22 @@ class Job extends Model
     // you can specify the table name explicitly
     protected $table = 'job_listings';
  
-    protected $fillable = ['title', 'description'];
+    protected $fillable = [        'title',
+    'description',
+    'salary',
+    'tags',
+    'job_type',
+    'remote',
+    'requirements',
+    'benefits',
+    'address',
+    'city',
+    'state',
+    'zipcode',
+    'contact_email',
+    'contact_phone',
+    'company_name',
+    'company_description',
+    'company_logo',
+    'company_website'];
 }
