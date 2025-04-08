80af41c319ac0a09e768044e4d72044f4efdb34d
Kristian <10221425@ucn.dk>
2025-04-08 08:25:37 +0200

    Model Binding & Single Job Listing

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,9 +2,9 @@
   <h1>Available Jobs</h1>
   <ul>
     @forelse($jobs as $job)
-      <li>{{ $job->title }} - {{ $job->description }}</li>
+    <li><a href="{{route('jobs.show', $job->id)}}">{{$job->title}}</a> - {{$job->description}}</li>
     @empty
-      <li>No jobs available</li>
+    <li>No jobs available</li>
     @endforelse
   </ul>
 </x-layout>
