318cf4683eaa876de058ef9449e6c177d1f1bad1
Kristian <10221425@ucn.dk>
2025-04-12 10:10:11 +0200

    Creating Factories & Faker Library

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
+    use HasFactory;
+
     // If the table name is not the plural form of the model name
     // you can specify the table name explicitly
     protected $table = 'job_listings';
