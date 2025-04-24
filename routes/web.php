67d1094c0c45134b558b1feec2b3bae95d4ab9c9
Kristian <10221425@ucn.dk>
2025-04-24 11:42:32 +0200

    Guest Middleware & Groups

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ Route::resource('jobs', JobController::class)->middleware('auth')->only(['create
 // Define the rest of the resource routes without middleware
 Route::resource('jobs', JobController::class)->except(['create', 'edit', 'update', 'destroy']);
 
-Route::get('/register', [RegisterController::class, 'register'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+Route::middleware('guest')->group(function () {
+  Route::get('/register', [RegisterController::class, 'register'])->name('register');
+  Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+  Route::get('/login', [LoginController::class, 'login'])->name('login');
+  Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
+});
+
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
