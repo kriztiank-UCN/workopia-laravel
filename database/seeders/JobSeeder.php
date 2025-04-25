ddde0892b633a033aa5a54b6faf0370f9fcecb0a
Kristian <10221425@ucn.dk>
2025-04-25 09:16:27 +0200

    Test User Seeder

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -17,12 +17,21 @@ class JobSeeder extends Seeder
         // Load job listings data
         $jobListings = include database_path('seeders/data/job_listings.php');
 
-        // Get all user IDs
-        $userIds = User::pluck('id')->toArray();
+        // Get the ID of the user created by TestUserSeeder
+        $testUserId = User::where('email', 'test@test.com')->value('id');
+
+        // Get all other user IDs from user model, except the one created by TestUserSeeder
+        $userIds = User::where('email', '!=', 'test@test.com')->pluck('id')->toArray();
+
         // ampersand (&) is used to pass by reference
-        foreach ($jobListings as &$listing) {
-            // Assign a random user_id to each job listing
-            $listing['user_id'] = $userIds[array_rand($userIds)];
+        foreach ($jobListings as $index => &$listing) {
+            if ($index < 2) {
+                // Assign the first two job listings to the test user
+                $listing['user_id'] = $testUserId;
+            } else {
+                // Assign the rest to random users
+                $listing['user_id'] = $userIds[array_rand($userIds)];
+            }
             // Add timestamps
             $listing['created_at'] = now();
             $listing['updated_at'] = now();
