81ff78a7abbfc84bd4f6dc5d16df82c0ed442bf6
Kristian <10221425@ucn.dk>
2025-04-27 08:25:27 +0200

    Profile Controller & Update User Info

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,37 +1,43 @@
 <x-layout>
-<div class="bg-white p-8 rounded-lg shadow-md w-full">
-  <h3 class="text-3xl text-center font-bold mb-4">My Job Listings</h3>
-  @forelse ($jobs as $job)
-  <div
-    class="flex justify-between items-center border-b-2 border-gray-200 py-2"
-  >
-    <div>
-      <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
-      <p class="text-gray-700">{{ $job->job_type }}</p>
-    </div>
-   <div class="flex space-x-4">
-      <a
-        href="{{ route('jobs.edit', $job->id) }}"
-        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded text-sm"
-        >Edit</a
-      >
-      <form
-        method="POST"
-        action="{{ route('jobs.destroy', $job->id) }}?from=dashboard"
-        onsubmit="return confirm('Are you sure you want to delete this job?');"
-      >
-        @csrf @method('DELETE')
-        <button
-          type="submit"
-          class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded text-sm"
-        >
-          Delete
-        </button>
+  <section class="flex flex-col gap-6 md:flex-row">
+    {{-- Profile Info Form --}}
+    <div class="w-full rounded-lg bg-white p-8 shadow-md md:w-1/2">
+      <h3 class="mb-4 text-center text-3xl font-bold">Profile Info</h3>
+      <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        {{-- component fields --}}
+        <x-inputs.text id="name" name="name" label="Name" value="{{ $user->name }}" />
+        <x-inputs.text id="email" name="email" label="Email address" type="email" value="{{ $user->email }}" />
+        <button type="submit"
+          class="my-3 w-full rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600 focus:outline-none">Save</button>
       </form>
     </div>
-  </div>
-  @empty
-  <p class="text-gray-700">You have no job listings.</p>
-  @endforelse
-</div>
+    {{-- Job Listings --}}
+    <div class="w-full rounded-lg bg-white p-8 shadow-md">
+      <h3 class="mb-4 text-center text-3xl font-bold">My Job Listings</h3>
+      @forelse ($jobs as $job)
+        <div class="flex items-center justify-between border-b-2 border-gray-200 py-2">
+          <div>
+            <h3 class="text-xl font-semibold">{{ $job->title }}</h3>
+            <p class="text-gray-700">{{ $job->job_type }}</p>
+          </div>
+          <div class="flex space-x-4">
+            <a href="{{ route('jobs.edit', $job->id) }}"
+              class="rounded bg-blue-500 px-4 py-2 text-sm text-white hover:bg-blue-600">Edit</a>
+            <form method="POST" action="{{ route('jobs.destroy', $job->id) }}?from=dashboard"
+              onsubmit="return confirm('Are you sure you want to delete this job?');">
+              @csrf @method('DELETE')
+              <button type="submit" class="rounded bg-red-500 px-4 py-2 text-sm text-white hover:bg-red-600">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+      @empty
+        <p class="text-gray-700">You have no job listings.</p>
+      @endforelse
+    </div>
+  </section>
+  <x-bottom-banner />
 </x-layout>
