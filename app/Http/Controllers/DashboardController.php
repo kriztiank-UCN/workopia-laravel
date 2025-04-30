8fbfb3664e2a6ad3be297aab7b143dd49dca6527
Kristian <10221425@ucn.dk>
2025-04-30 10:39:23 +0200

    Show Applications To Owner

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,8 +16,8 @@ class DashboardController extends Controller
         // Get logged in user
         $user = Auth::user();
 
-        // Get the user listings
-        $jobs = Job::where('user_id', $user->id)->get();
+        // Get the user info and listings
+        $jobs = Job::where('user_id', $user->id)->with('applicants')->get();
 
         return view('dashboard.index', compact('user', 'jobs'));
     }
