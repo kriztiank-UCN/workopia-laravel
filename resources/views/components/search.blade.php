f2803ca55d116b9b547ac99c2a6b3329d28a5666
Kristian <10221425@ucn.dk>
2025-05-02 10:42:52 +0200

    Search Functionality (LIKE QUERY)

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,8 +1,8 @@
 <form method="GET" action="{{ route('jobs.search') }}" class="mx-5 block md:mx-auto md:space-x-2">
-  <input type="text" name="keywords" placeholder="Keywords"
-    class="w-full bg-white px-4 py-3 focus:outline-none md:w-72" />
+  <input type="text" name="keywords" placeholder="Keywords" class="w-full bg-white px-4 py-3 focus:outline-none md:w-72"
+    value="{{ request('keywords') }}" />
   <input type="text" name="location" placeholder="Location"
-    class="w-full bg-white px-4 py-3 focus:outline-none md:w-72" />
+    class="w-full bg-white px-4 py-3 focus:outline-none md:w-72" value="{{ request('location') }}" />
   <button class="w-full bg-blue-700 px-4 py-3 text-white hover:bg-blue-600 focus:outline-none md:w-auto">
     <i class="fa fa-search mr-1"></i> Search
   </button>
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,6 +2,14 @@
   <div class="mb-4 flex h-24 items-center justify-center rounded bg-blue-900 px-4">
     <x-search />
   </div>
+  {{-- check for any search parameters in the URL and if there are any, it will display a back button --}}
+  @if (request()->has('keywords') || request()->has('location'))
+    <!-- Back Button -->
+    <a href="{{ route('jobs.index') }}"
+      class="mb-4 inline-block rounded bg-gray-700 px-4 py-2 text-white hover:bg-gray-600">
+      <i class="fa fa-arrow-left mr-1"></i> Back
+    </a>
+  @endif
 
   <div class="mb-6 grid grid-cols-1 gap-4 md:grid-cols-3">
     @forelse($jobs as $job)
